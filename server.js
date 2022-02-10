40d3fb230bdb77d0eff131deffcbb2dde1206511
fatahfreinds <99268671+fatahfreinds@users.noreply.github.com>
2022-02-10 18:14:06 +0530

    .

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
+const serverless = require("serverless-http");
 const path = require('path');
 
 const connectDB = require('./server/database/connection');
 
 const app = express();
+const router= express.router();
 
 dotenv.config( { path : 'config.env'} )
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3471
 
 // log requests
 app.use(morgan('tiny'));
@@ -24,6 +26,8 @@ app.use(bodyparser.urlencoded({ extended : true}))
 app.set("view engine", "ejs")
 //app.set("views", path.resolve(__dirname, "views/ejs"))
 
+app.use('/.netlify/functions/server', router);
+
 // load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
@@ -33,4 +37,7 @@ app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 app.use('/', require('./server/routes/router'))
 app.use('*',(req,res)=>{res.render('invalid')})
 
-app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+// app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+
+module.exports = app;
+module.exports.handler = serverless(app);
